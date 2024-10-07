51303200141900348f6aab45c0a213d23a55e072
Per Kleven <per@tightship.se>
2024-10-07 13:31:39 +0200

    add release script

---
+++ scripts/release.js
@@ -0,0 +1,35 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+import packageJson from "../package.json" assert { type: "json" };
+
+const execPromise = promisify(exec);
+
+async function runCommand(command, options = {}) {
+  try {
+    console.log(`Executing command: ${command}`);
+    const { stdout, stderr } = await execPromise(command, options);
+    if (stdout) {
+      console.log(`${stdout}`);
+    }
+    if (stderr) {
+      console.warn(`${stderr}`);
+    }
+  } catch (error) {
+    console.error(`Error executing command: ${error.message}`);
+  }
+}
+
+const version = packageJson.version;
+
+console.log(`Version: ${version}`);
+
+await runCommand(`npm run build`);
+
+// add to git
+await runCommand("git add .");
+await runCommand(`git commit -m "release ${version}"`);
+await runCommand(`git tag v${version}`);
+await runCommand(`git push --follow-tags `);
+
+// publish to npm registry
+await runCommand(`npm publish`);
