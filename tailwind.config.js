ebd22f6046f5d2d7fa7f93e26f882dbc706115e4
Per Kleven <per@tightship.se>
2024-11-15 17:40:36 +0100

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./main.jsx", "./src/**/*.{jsx}"],
+  content: ["./main.jsx", "./src/**/*.jsx"],
   theme: {
     extend: {},
   },
