865e876027b6b82dde30d747cc8ef5c4a64d4ec9
Per Kleven <per@tightship.se>
2024-11-21 13:29:14 +0100

    add hello world template + remove video

---
+++ public/templates/hello-world.js
@@ -0,0 +1,43 @@
+const defaultParams = {
+  name: "Unknown",
+  fps: 30,
+  size: {
+    width: 1920,
+    height: 1080,
+  },
+};
+
+const template = async ({ canvas, timeline, params, utils, fabricUtils }) => {
+  canvas.set({ backgroundColor: "#0000ee" });
+
+  const text = await fabricUtils.loadText({
+    text: `Hello ${params.name}!`,
+    options: {
+      fontSize: 200,
+      fontFamily: "sans-serif",
+      fill: "white",
+      originX: "center",
+      originY: "center",
+    },
+  });
+
+  canvas.add(text);
+  canvas.centerObject(text);
+
+  timeline.from(text, {
+    scaleX: 0,
+    scaleY: 0,
+    duration: 0.5,
+    delay: 0.2,
+    ease: "elastic",
+  });
+
+  timeline.to(text, {
+    opacity: 0,
+    duration: 2,
+    delay: 1,
+    ease: "expo.out",
+  });
+};
+
+export { template, defaultParams };
