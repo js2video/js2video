8ee070cd59f5c8eea69412bed5824041bb03f19e
Per Kleven <per@tightship.se>
2024-11-22 12:37:58 +0100

    improve react export

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import { externalizeDeps } from "vite-plugin-externalize-deps";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [externalizeDeps()],
+  plugins: [react(), externalizeDeps()],
   build: {
     lib: {
       entry: resolve(__dirname, "./src/index.js"),
