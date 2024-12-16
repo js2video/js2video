b546ea63e70b54370216d02cf31506efa5a1b7de
Per Kleven <per@tightship.se>
2024-12-16 18:15:26 +0100

    add mona sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./play/index.{jsx,html}", "./index.html", "./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ui: ["var(--font-ui)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
