f7dae7ba21edad9a6211309c0bea67c95cc746cf
pkl <per@tightship.se>
2025-09-09 08:52:37 +0200

    change bgcolor

--- src/lib/components/js2video-controls.jsx
+++ src/lib/components/js2video-controls.jsx
@@ -255,7 +255,7 @@ const ExportButton = () => {
       {!!isExporting && (
         <div
           id="exporting"
-          className="absolute inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+          className="absolute inset-0 z-50 flex items-center justify-center bg-black/50"
         >
           <div className="flex flex-col items-center gap-4">
             <div>
